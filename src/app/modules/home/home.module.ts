118dbcbbbe3f1d025c797175ca585cc36cc5c812
DESKTOP-JAF3RCI\SakibSeaum <Sakib.Mahmood@brainstation-23.com>
2021-09-14 14:41:14 +0600

    Created two button for expense & income .. Also Added route navigations

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -8,9 +9,11 @@ import { HomeComponent } from './home.component';
 @NgModule({
   declarations: [
     HomeComponent
+
   ],
   imports: [
     CommonModule,
+    MatButtonModule,
     HomeRoutingModule
   ]
 })
