626353109ad0e812e59b0d03388d58be5c65b725
DESKTOP-JAF3RCI\SakibSeaum <Sakib.Mahmood@brainstation-23.com>
2021-09-15 02:26:44 +0600

    added a delete option to remove a specific data & calculated income amount in ts file.

--- src/app/modules/income/income.component.ts
+++ src/app/modules/income/income.component.ts
@@ -9,16 +9,16 @@ import { AccountService } from 'src/app/core/account.service';
 })
 export class IncomeComponent implements OnInit {
   private incomeData!: Data[];
-  public retrieveData: Data[];
-  
+  public retrieveData!: Data[];
+  storage = 'incomeStorage';
+  totalIncome = 0;
 
   constructor(private accountService: AccountService) {
     this.incomeData = [
       new Data("Income01", 1000),
-      new Data("Income02", 2000),
     ];
-    this.accountService.setLocatStorageDatas(this.incomeData, 'incomeStorage');
-    this.retrieveData = this.accountService.getDatas('incomeStorage');
+    this.totalIncome = 1000;
+    this.updateLocalStorage();
    }
 
   ngOnInit(): void {
@@ -29,9 +29,29 @@ export class IncomeComponent implements OnInit {
     var numberamount: number = +amount
     //console.log(text, numberamount);
     this.incomeData.push(this.accountService.addData(text, numberamount));
-
-    this.accountService.setLocatStorageDatas(this.incomeData, 'incomeStorage');
-    this.retrieveData = this.accountService.getDatas('incomeStorage');
+    this.totalIncome = this.totalIncome + this.incomeData[this.incomeData.length - 1 ].amount ; 
+    this.updateLocalStorage();
     //console.log(this.retrieveData);
   }
+
+  removeIncomeItem(text: any){
+    const index = this.accountService.removeDatas(text,this.storage);
+    if (index > -1) {
+      this.totalIncome = this.totalIncome - this.incomeData[index].amount ; 
+      this.incomeData.splice(index,1);
+      this.updateLocalStorage();
+
+    }
+  }
+
+  updateLocalStorage(){
+
+    this.accountService.setLocatStorageDatas(this.incomeData, this.storage);
+    this.retrieveData = this.accountService.getDatas(this.storage);
+
+    //this.totalIncome = this.totalIncome+this.retrieveData[this.retrieveData.length -1 ].amount;
+    //console.log(this.totalIncome);
+  }
+
+  
 }
