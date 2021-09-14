4dfc330d59f4dae039fd6d68b3908edfce845170
DESKTOP-JAF3RCI\SakibSeaum <Sakib.Mahmood@brainstation-23.com>
2021-09-14 20:06:46 +0600

    Created a service names account service

---
+++ src/app/modules/account.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Data } from  '../classes/data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  
+
+  constructor() { 
+    
+  }
+
+  public addData(text: string, amount: number) : any {
+    let data = new Data(text,amount);
+    return data;
+    
+  }
+
+  public setLocatStorageDatas(datas: Data[], storage: string):void {
+    localStorage.setItem(storage,JSON.stringify({datas: datas}));
+  }
+
+  public getDatas(storage: string): Data[] {
+    let localStorageItem = JSON.parse(localStorage.getItem(storage) as any);
+    return localStorageItem
+  }
+}
