99efe6ed4d0188de13366e1cd05ecdd7e5abd91b
DESKTOP-JAF3RCI\SakibSeaum <Sakib.Mahmood@brainstation-23.com>
2021-09-15 16:32:10 +0600

    cleaning file

--- src/app/core/models/data.ts
+++ src/app/core/models/data.ts
@@ -2,6 +2,3 @@ export interface Data {
     text: string;
     amount: number
 }
-
-  //convert from class to interface
-  //create a folder in core and store the interface in created folder named models.
