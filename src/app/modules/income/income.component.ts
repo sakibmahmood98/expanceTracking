1377c1bbf98c928b2c2f571f2b57a176c69cf886
DESKTOP-JAF3RCI\SakibSeaum <Sakib.Mahmood@brainstation-23.com>
2021-09-17 00:56:42 +0600

    Styled button & input in Income form.

--- src/app/modules/income/income.component.ts
+++ src/app/modules/income/income.component.ts
@@ -23,8 +23,8 @@ export class IncomeComponent implements OnInit {
   constructor(private service: StorageService) {
 
     this.incomeData = [
-      {text: 'Income01', amount: 2000},
-      {text: 'Income02', amount: 3000},
+      {text: 'Income01', amount: 2000, month: '01', year: '2021'},
+      {text: 'Income02', amount: 3000, month: '01', year: '2021'},
 
     ];
     this.totalIncome = 5000;
@@ -40,7 +40,7 @@ export class IncomeComponent implements OnInit {
     let text = this.form.value.text;
     var numberamount: number = +this.form.value.amount
 
-    this.incomeData.push(this.service.addData(text, numberamount));
+    this.incomeData.push(this.service.addData(text, numberamount,' '));
     this.totalIncome = this.totalIncome + this.incomeData[this.incomeData.length - 1 ].amount ; 
     this.updateLocalStorage();
 
